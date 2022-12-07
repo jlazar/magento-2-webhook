a8e6e0c843e7ea31f03efbf9b61328bb0c105760
Jon Lazarowicz <jon@slytrunk.com>
2022-12-07 10:05:00 -0500

    revert

--- Helper/Data.php
+++ Helper/Data.php
@@ -270,33 +270,6 @@ class Data extends CoreHelper
             if ($item instanceof Product) {
                 $item->setStockItem(null);
             }
-            
-            if ($item->getAllItems()) {
-                /** @var \Magento\Sales\Model\Order $order */
-                $order = $this->objectManager->create(\Magento\Sales\Model\Order::class)->load($item->getId());
-                /** @var \Magento\Sales\Api\Data\OrderItemInterface $orderItem */
-                $allOrderItems = $order->getItems();
-                $item->setData('itemData', $item->getId());
-                
-                $orderItems = [];
-                $item->setData('loop', 'no');
-
-                foreach ($allOrderItems as $orderItem) {
-                    $item->setData('loop', 'yes');
-
-                    $orderItem->setData('order_item_id', $orderItem->getItemId());
-                    $orderItem->setData('order_item_id2', $orderItem->getId());
-                    // $orderItem->setData('key', $key);
-                    $orderItems[] = $orderItem->getData();
-                    $item->setData('itemData', print_r($orderItem->getData(), true));
-                }
-                $item->setData('orderItems', $orderItems);
-
-                foreach($item->getAllItems() as $key => $oItem) {
-                    $oItem.setData('order_item_id', $key);
-                    $item->setData('itemData', $key);
-                }
-            }
 
             if ($item->getShippingAddress()) {
                 $item->setData('shippingAddress', $item->getShippingAddress()->getData());
